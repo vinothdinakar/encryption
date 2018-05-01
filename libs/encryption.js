ffc7ad0394f776d595d4d65733291ec69ffe40df
ivc13020adm <stash_admin@bitbucket.org>
2018-05-01 11:31:30 -0500

    Modifying decipher

--- libs/encryption.js
+++ libs/encryption.js
@@ -4,31 +4,26 @@ var crypto = require('crypto');
 var encryption = module.exports = new function () {
 
     var algorithm = 'aes256';
-
+    var inputEncoding = 'utf8';
+    var outputEncoding = 'hex';
 
     this.encrypt = function (text, key) {
-        console.log('Encrypting with ' + key + ' using algorithm ' + algorithm);
+        console.log('Encrypting with key - ' + key + ' using algorithm ' + algorithm);
 
-        var inputEncoding = 'utf8';
-        var outputEncoding = 'hex';
-
-        var myKey = crypto.createCipher(algorithm, key);
-        var encryptedText = myKey.update(text, inputEncoding, outputEncoding);
-        encryptedText += myKey.final(outputEncoding);
+        var encryptor = crypto.createCipher(algorithm, key);
+        var encryptedText = encryptor.update(text, inputEncoding, outputEncoding);
+        encryptedText += encryptor.final(outputEncoding);
 
         return encryptedText;
     };
 
     this.decrypt = function (encryptedText, key) {
-        console.log('Decrypting with ' + key + ' using algorithm ' + algorithm);
+        console.log('Decrypting with key - ' + key + ' using algorithm ' + algorithm);
 
-        var inputEncoding = 'hex';
-        var outputEncoding = 'utf8';
-
-        var myKey = crypto.createCipher(algorithm, key);
-        var decryptedText = myKey.update(encryptedText, inputEncoding, outputEncoding);
+        var decryptor = crypto.createDecipher(algorithm, key);
+        var decryptedText = decryptor.update(encryptedText, outputEncoding, inputEncoding);
         console.log(decryptedText);
-        decryptedText += myKey.final(outputEncoding);
+        decryptedText += decryptor.final(inputEncoding);
 
         return decryptedText;
     };
